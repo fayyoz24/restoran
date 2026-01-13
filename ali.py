8380a40ef04aa2aed1d3bc26c554f80971e1c781
fayyoz24 <fayyozusmon@gmail.com>
2026-01-13 16:44:50 +0500

    created ALI

---
+++ ali.py
@@ -0,0 +1,15 @@
+class Ali:
+    def __init__(self, kurtka, yosh, jinsi, millat):
+        self.kurtka = kurtka
+        self.yosh = yosh
+        self.jins = jinsi
+        self.millat = millat
+        
+    def ishlamoq(self):
+
+        if self.yosh >= 18:
+            return "Yaxshi Ishlang"
+        return "Sizga ish mumkinmas!"
+    
+    def __str__(self):
+        return f"{self.yosh}-{self.jins}-{self.millat}"
